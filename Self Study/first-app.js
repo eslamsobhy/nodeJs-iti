4d0354d129a726542f5818f7f4ca945866b1d78e
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-14 18:48:36 +0300

    created a server and parsed the data from the request

---
+++ Self Study/first-app.js
@@ -0,0 +1,4 @@
+const fs = require("fs");
+
+console.log("Hello from Node.js");
+fs.writeFileSync("test.txt", "Hello from Node.js!");
---
+++ Self Study/server.js
@@ -0,0 +1,46 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  //   console.log(req.url, req.method, req.headers);
+  //   process.exit(); // Stopped the server
+
+  const url = req.url;
+  const method = req.method;
+
+  res.setHeader("Content-Type", "text/html");
+
+  if (url === "/") {
+    res.write(`
+        <form action="/message" method="POST">
+            <input type="text" name="message"/>
+            <input type="submit" value="Send">
+        </form>
+    `);
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunck) => {
+      //   console.log(chunck);
+      body.push(chunck);
+    });
+
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      //   console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.write(`<h1>Hello There!</h1>`);
+  res.end();
+});
+
+server.listen(8000);

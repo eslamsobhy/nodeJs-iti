aa0255805ea636784620cf2811745c62a3c602e6
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-20 00:12:12 +0300

    spinned the server and sent back our first express response

--- Self Study/Express/index.js
+++ Self Study/Express/index.js
@@ -1,1 +1,15 @@
-console.log("Hello from the new project!");
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("Hello from the middleware");
+  next(); // allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+  console.log("Hello from another middleware!!");
+  res.send("<h1>Hello from Express.js</h1>"); // send response
+});
+
+app.listen(8000);

b2e108f54aa39cce0af605c2bfc02fa2577677a1
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-20 02:18:59 +0300

    added multiple middlewares

--- Self Study/Express/index.js
+++ Self Study/Express/index.js
@@ -3,12 +3,17 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-  console.log("Hello from the middleware");
-  next(); // allows the request to continue to the next middleware
+  console.log("This will always run for each request!");
+  next();
 });
 
-app.use((req, res, next) => {
-  console.log("Hello from another middleware!!");
+app.use("/add", (req, res, next) => {
+  console.log("will just run for '/add' request!");
+  res.send("<h1>The adding page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("Hello from middleware!!");
   res.send("<h1>Hello from Express.js</h1>"); // send response
 });
 

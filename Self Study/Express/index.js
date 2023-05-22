dff0eb3eb85e993ec0137bc3e6d059925b5948aa
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-05-22 18:22:27 +0300

    added the 404 error page

--- Self Study/Express/index.js
+++ Self Study/Express/index.js
@@ -13,4 +13,11 @@ app.use(bodyParser.json());
 app.use(adminRouter);
 app.use(shopRouter);
 
+// handling the not found routes
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .send(`<h1 style="text-align: center; color: red">Page not found!</h1>`);
+});
+
 app.listen(8000);
